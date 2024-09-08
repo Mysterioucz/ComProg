6b8498814dcdd472dccc4d11af3887cad816db22
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-09-09 00:00:17 +0700

    idk

--- test.py
+++ test.py
@@ -1,25 +1,8 @@
-"""Donut"""
-import math
-def main():
-    """Donut"""
-    a = abs(int(input()))
-    b = abs(int(input()))
-    c = abs(int(input()))
-    d = abs(int(input()))
-    if not a and not b and not c and not d:
-        print("0", "0")
-    elif d < b:
-        print(d * a, d)
-    elif not c:
-        print(d * a, d)
-    elif not d:
-        print(d, c)
-    else:
-        setdonut = b + c
-        cost = b * a
-        mindonut = d // setdonut
-        total = mindonut * cost
-        alldonut = (mindonut * setdonut) + (d-(mindonut * setdonut))
-        print(total, alldonut)
-
-main()
+x = input('input ')
+if x.isdigit():
+    print('digit')
+else:
+    if x in "AEIOU":
+        print('vowel');
+    elif x.isupper():
+        print('capital letter')
