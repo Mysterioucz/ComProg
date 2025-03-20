fc82004544e463f798753e352fde194be68679e1
Mysterioucz <chatrinza@gmail.com>
2025-03-20 09:13:08 +0700

    update some code

--- TA/Quiz-1-2567/snakeladder.py
+++ TA/Quiz-1-2567/snakeladder.py
@@ -7,7 +7,7 @@ def create_map(map_index,n,col):
                 map_index[i][j] = (i + 1)*col - j
 
 n = int(input())
-map_index = []
+map_index = [] # [[1,2,3,4],[5,6,7,8]]
 # print(map_index)
 
 snake_pos = [] ;ladder_pos = [] ;snake_dest = []; ladder_dest = []
@@ -24,16 +24,19 @@ for i in range(n-1, -1, -1):
         elif "L" in map[j] :
             ladder_pos.append(map_index[i][j])
             ladder_dest.append(int(map[j][1:]))
+
 # print(snake_pos, snake_dest)
 # print(ladder_pos, ladder_dest)
 move = [int(e) for e in input().split()]
 cur_pos = 0;
 for i in range(len(move)):
     cur_pos += move[i]
+    
     if cur_pos in snake_pos:
         cur_pos = snake_dest[snake_pos.index(cur_pos)]
     elif cur_pos in ladder_pos:
         cur_pos = ladder_dest[ladder_pos.index(cur_pos)]
+        
     if cur_pos >= len(map)*n:
         print("win", end = " ")
         break
